e63b0b174648d5297426256e146f8622dd4ffb59
RobOutlook <robfuncke@outlook.com>
2018-06-02 12:25:40 +0200

    Fixed file

--- FirstProject/src/Main.java
+++ FirstProject/src/Main.java
@@ -10,7 +10,7 @@ public class Main {
 	}
 
 	public static void displayGreeting() {
-		System.out.println("Hi people");
+		System.out.println("Hi people nog");
 		System.out.println("En nog een regel");
 		System.out.println("Test1");
 		System.out.println("Test2");
