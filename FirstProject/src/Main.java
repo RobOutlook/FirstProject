2bff5520a7130402914036f9cbb973964132dee8
RobOutlook <robfuncke@outlook.com>
2018-06-02 11:08:02 +0200

    Initial checkin

---
+++ FirstProject/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		SalesData data = new SalesData();
+		displayGreeting();
+		data.display();
+
+	}
+
+	public static void displayGreeting() {
+		System.out.println("Hi people");
+		System.out.println("En nog een regel");
+	}
+}
